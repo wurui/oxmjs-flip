d3b14dd94c7d78da9f37e60ae5009cdb96bb6b2c
wurui <wurui.wr@gmail.com>
2016-05-30 20:34:54 +0800

    almost OK in safari

--- asset/index.js
+++ asset/index.js
@@ -11,35 +11,43 @@ define(['zepto'],function(undef){
     var startX,startY,deltaX=0,deltaY= 0,
         that=this,
         wrap=this.wrap,
-        screenWidth= $('body').on('touchstart touchend touchmove',function(e){
-      //console.log('touch', e.type,e)
-      var touch0= e.touches[0];
-      switch (e.type){
-        case 'touchstart':
-            startX=touch0.clientX;
-            startY=touch0.clientY;
-              break
-        case 'touchend':
-            //console.log('de',deltaX)
-            if(Math.abs(deltaX)>screenWidth/2){
-             // console.log('gogogo',deltaX)
-              that.go(-deltaX);
-            }
-          deltaX=0;
-          deltaY=0;
-          break
-        case 'touchmove':
-            deltaX=touch0.clientX;
-            deltaY=touch0.clientY;
-          var percent= -100* deltaX/screenWidth/2//2 为阻尼系数
-          //console.log('precent',percent)
 
-          wrap.children('.flip-window').css({
-            transform:'translate('+percent+'%, 0)'
-          })
-            //console.log('moving',deltaX,deltaY)
-          break
-      }
+        screenWidth= $('body').on('touchstart touchend touchmove',function(e){
+          //console.log('touch', e.type,e)
+          var touch0= e.touches[0];
+          switch (e.type){
+            case 'touchstart':
+                startX=touch0.clientX;
+                startY=touch0.clientY;
+                  break
+            case 'touchend':
+                //console.log('de',deltaX)
+                wrap.children('.flip-window').removeAttr('style')
+                if(Math.abs(deltaX)>screenWidth/2){
+                 // console.log('gogogo',deltaX)
+                  that.go(-deltaX);
+                    /**
+                     * todo: go 从滑动点开始，而不是从起点开始
+                     * */
+                }
+              deltaX=0;
+              deltaY=0;
+              break
+            case 'touchmove':
+                deltaX=touch0.clientX-startX;
+                deltaY=touch0.clientY-startY;
+//console.log('point',that.currentIndex -  deltaX/screenWidth)
+              var percent= -100*(that.currentIndex -  deltaX/screenWidth)//2 为阻尼系数
+              //console.log('precent',percent)
+
+              wrap.children('.flip-window').css({
+                transform:'translate('+percent+'%, 0)'
+              })
+                //console.log('moving',deltaX,deltaY)
+              break
+          }
+
+            return false
 
 
     }).width();
@@ -65,7 +73,7 @@ define(['zepto'],function(undef){
       if(!this.count) return this;
 
       direction=(direction||1)>0?1:-1;
-console.log('direction',direction)
+//console.log('direction',direction)
       //this.currentIndex+;
 
       this.currentIndex=Math.max(0,Math.min(this.count-1,this.currentIndex+direction))
--- mod.xsl
+++ mod.xsl
@@ -3,6 +3,14 @@
     <xsl:template match="/root" name="oxmjs-flip">
     <!-- className 'J_OXMod' required  -->
     <div class="J_OXMod oxmod-oxmjs-flip" ox-mod="oxmjs-flip">
+        <!--
+        <div style="width:100%;height:100px;border:solid 1px red;white-space:nowrap;overflow:auto;">
+            <span style="display:inline-block;height:100px;width:100%;">1</span>
+            <span style="display:inline-block;height:100px;width:100%;">2</span>
+            <span style="display:inline-block;height:100px;width:100%;">3</span>
+            <span style="display:inline-block;height:100px;width:100%;">4</span>
+        </div>
+        -->
         <div data-on="1">
             <div class="flip-window">
                 <div class="flip-pages" style="width:{count(data/icon-menu/i)}00%;">
