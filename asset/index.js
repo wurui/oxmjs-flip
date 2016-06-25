a5da5989753357fefa6dae73a6ee8b7740a24e63
rio84 <rio84@qq.com>
2016-06-25 10:15:54 +0800

    autoplay supoort

--- asset/index.js
+++ asset/index.js
@@ -27,6 +27,7 @@ define(['zepto'],function(undef){
                     }
                   deltaX=0;
                   deltaY=0;
+                    that.operating=false;
                   break
                 case 'touchmove':
                     deltaX=touch0.clientX-startX;
@@ -43,6 +44,7 @@ define(['zepto'],function(undef){
         }).width();
 
         wrap.on('touchstart',function(e){
+            that.operating=true;
             var touch0= e.touches[0];
             startX=touch0.clientX;
             startY=touch0.clientY;
@@ -66,23 +68,39 @@ define(['zepto'],function(undef){
         go:function(direction){
             if(!this.count) return this;
             direction=(direction||1)>0?1:-1;
-            this.currentIndex=Math.max(0,Math.min(this.count-1,this.currentIndex+direction))
-            this.wrap.attr('data-on',(this.currentIndex+1));
+
+            var currentIndex=this.wrap.attr('data-on')-1;
+
+            currentIndex=Math.max(0,Math.min(this.count-1,currentIndex+direction))
+            this.currentIndex=currentIndex;
+
+            this.wrap.attr('data-on',currentIndex+1);
         },
         currentIndex:0,
+        playCounter:0,
         play:function(){
             if(!this.count) return this;
-            this.currentIndex++;
-
-            this.currentIndex=this.currentIndex%this.count;
-
-            this.wrap.attr('data-on',(this.currentIndex+1));
 
             var _this=this;
 
-            setTimeout(function(){
-                _this.play();
-            },3000)
+
+            setTimeout(function(){_this.play()},100);
+
+            if(this.operating){
+                this.playCounter=0;
+                return this
+            }
+
+            if((++this.playCounter) > 30){
+                this.playCounter=0;
+                var currentIndex=_this.wrap.attr('data-on')-1;
+                currentIndex=(++currentIndex)%_this.count;
+
+                _this.currentIndex=currentIndex;
+
+                _this.wrap.attr('data-on',(currentIndex+1));
+            }
+
 
             return this;
         }
@@ -100,6 +118,7 @@ define(['zepto'],function(undef){
         conf.wrap=conf.wrap|| mod.children('div');
 
         return new constructor(conf)
+
 
     }
   }
